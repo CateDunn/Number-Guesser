aa9b646c86d068978d39f1c6fb5d05796b4be668
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-09-13 15:45:35 -0400

    adding variables, event listener and message function

---
+++ app.js
@@ -0,0 +1,36 @@
+//Game Values
+
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+//Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//Listen for Guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value)
+
+  //Validate Input
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please Enter a Number between ${min} and ${max}`, 'red');
+  }
+
+});
+
+//Set Message Function
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+  
+}
